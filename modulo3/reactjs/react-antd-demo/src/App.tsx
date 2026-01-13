3f0586b8a8114842dee9f3382f09346edccf98e8
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-01-13 10:01:34 -0500

    Clase 13-01-2025

---
+++ modulo3/reactjs/react-antd-demo/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Importa SOLO uno a la vez:
+import LabAntButtons from "./lab/LabAntButtons";
+
+
+export default function App() {
+  return 
+  <LabAntButtons />;
+}
